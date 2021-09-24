3da0776f2ba9cf30b7c633a0ccc5b94ef3ba3a14
ScratchnestMPU <aditya@scratchnest.com>
2021-09-24 20:21:34 +0530

    Demo

--- gdmApp/gdm.py
+++ gdmApp/gdm.py
@@ -2,14 +2,14 @@ from flask import render_template, request, redirect, session, url_for  #necessa
 import subprocess   #module import for dealing with execution of console command
 from gdmApp import app
 from .database import p1 as db
+import os
 
-#path=(__file__).split('/')
-#path.pop()
-#path.pop()
-#path.pop()
-#path="/".join(path)
-
-path='usr/sbin/certUploads/'
+path=(__file__).split('/')
+path.pop()
+path.pop()
+path.pop()
+path="/".join(path)
+path=path+'/certUploads/' 
 
 
 @app.route('/login',methods=['GET','POST'])   #route for handling login
@@ -33,7 +33,13 @@ def home():
         nodeData=db.getdata('Node')
         nodeData={'scanRate':nodeData[0][1],'status':nodeData[0][2]}
         cloudData=db.getdata('Cloud')
-        cloudData={'server':cloudData[0][1],'hostAdd':cloudData[0][2],'port':cloudData[0][3],'status':cloudData[0][4]}
+        server=cloudData[0][1]
+        serverType=''
+        if server=='custom':
+            serverType='Unsecured'
+        else:
+            serverType='Secured'
+        cloudData={'server':server,'serverType':serverType,'hostAdd':cloudData[0][2],'port':cloudData[0][3],'status':cloudData[0][4],'topic':cloudData[0][5],'pubFlag':cloudData[0][6]}
         return render_template('home.html',nodeData=nodeData,cloudData=cloudData)
     return redirect(url_for('login'))
 
@@ -63,7 +69,13 @@ def cloudConfig():
                 pvtKey.save(path+'key.pem.key')
                 iotCert.save(path+'cert.pem.crt')
         cloudData=db.getdata('Cloud')
-        cloudData={'server':cloudData[0][1],'hostAdd':cloudData[0][2],'port':cloudData[0][3],'status':cloudData[0][4]}
+        server=cloudData[0][1]
+        serverType=''
+        if server=='custom':
+            serverType='Unsecured'
+        else:
+            serverType='Secured'
+        cloudData={'server':server,'serverType':serverType,'hostAdd':cloudData[0][2],'port':cloudData[0][3],'status':cloudData[0][4],'topic':cloudData[0][5],'pubFlag':cloudData[0][6]}
         return render_template('cloudConfig.html',cloudData=cloudData)
     return redirect(url_for('login'))
 
@@ -71,10 +83,8 @@ def cloudConfig():
 def nodeConfig():
     if 'logedIn' in session:
         if request.method=="POST":
-            print('hahaha')
             db.updatetable('Node','ScaneRate',request.form['scanRate'])
             db.updatetable('Node','N_Status',request.form['status'])
-            print('it worked!')
         nodeData=db.getdata('Node')
         nodeData={'scanRate':nodeData[0][1],'status':nodeData[0][2]}
         return render_template('nodeConfig.html',nodeData=nodeData)
@@ -91,7 +101,7 @@ def debug():
     if 'logedIn' in session:
         cmdKey=request.args.get('cmd')          #extracting the command key from the html form
         if cmdKey:
-            cmd={'1':['hciconfig'],'2':['btmgmt','--index','0','info'],'3':['btmgmt','--index','0','find','-l']}
+            cmd={'1':['hciconfig'],'2':['btmgmt','--index','0','info'],'3':['btmgmt','--index','0','find','-l'],'4':['systemctl','status','apache2'],'5':['systemctl','status','app']}
             if cmdKey in cmd:
                 data=subprocess.Popen(cmd[cmdKey],stdout=subprocess.PIPE).communicate()[0]      #executing the command and getting the data into string format
                 data=data.decode('utf-8')                                                       #decoding the binary the data into string
@@ -110,7 +120,14 @@ def reports():
 
 @app.route('/dataManager')
 def dataManager():
+    if 'logedIn' in session: 
+        data=db.getdata('HistoricalData')
+        return render_template('dataManager.html',data=data,type='Historical Data')
+    return redirect(url_for('login'))
+
+@app.route('/dataManager/offData')
+def offlineData():
     if 'logedIn' in session:
         data=db.getdata('OfflineData')
-        return render_template('dataManager.html',data=data)
+        return render_template('dataManager.html',data=data,type='Offline Data')
     return redirect(url_for('login'))
